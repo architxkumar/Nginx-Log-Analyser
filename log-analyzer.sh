d8aa8eb2808d239e4f066f3b881caea55c19cd8c
Archit Kumar <architxkumar@proton.me>
2025-09-02 21:58:20 +0530

    feat: add request path, response code, user agent stats

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -4,3 +4,16 @@ printf "Top 5 IP addresses with the most requests:\n"
 awk '{print $1}' nginx-access.log | sort -t . -n -k 1,1 | uniq -c | sort -rn | head -n 5 > tempfile.txt
 awk '{print $2, "-", $1, "requests"}' tempfile.txt
 
+printf "\nTop 5 most requested paths:\n"
+awk '{print $7}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
+awk '{print $2, "-", $1, "requests"}' tempfile.txt
+
+printf "\nTop 5 response status codes:\n"
+awk '{print $9}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
+awk '{print $2, "-", $1, "requests"}' tempfile.txt
+
+printf "\nTop 5 user agents:\n"
+awk '{print $12}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
+awk '{print $2, "-", $1, "requests"}' tempfile.txt
+
+rm -f tempfile.txt
