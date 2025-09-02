aa56cf4e4c18279b0e42fda1c7cb5eb3de6b2783
Archit Kumar <architxkumar@proton.me>
2025-09-03 02:08:28 +0530

    refactor: remove usage of temporary `temp.file`

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 printf "Top 5 IP addresses with the most requests:\n"
-awk '{print $1}' nginx-access.log | sort -t . -n -k 1,1 | uniq -c | sort -rn | head -n 5 > tempfile.txt
-awk '{print $2, "-", $1, "requests"}' tempfile.txt
+awk '{print $1}' nginx-access.log | sort -t . -n -k 1,1 | uniq -c | sort -rn | head -n 5 | awk '{print $2, "-", $1, "requests"}'
 
 printf "\nTop 5 most requested paths:\n"
-awk '{print $7}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
-awk '{print $2, "-", $1, "requests"}' tempfile.txt
+awk '{print $7}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 | awk '{print $2, "-", $1, "requests"}'
 
 printf "\nTop 5 response status codes:\n"
-awk '{print $9}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
-awk '{print $2, "-", $1, "requests"}' tempfile.txt
+awk '{print $9}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 | awk '{print $2, "-", $1, "requests"}'
 
 printf "\nTop 5 user agents:\n"
-awk '{print $12}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 > tempfile.txt
-awk '{print $2, "-", $1, "requests"}' tempfile.txt
-
-rm -f tempfile.txt
+awk '{print $12}' nginx-access.log | sort | uniq -c | sort -rn | head -n 5 | awk '{print $2, "-", $1, "requests"}'
